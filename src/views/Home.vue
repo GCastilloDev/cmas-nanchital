492c05bf5cb00f11cf25060c5c916643844fb89e
Gustavo Castillo N <37945820+GustavoCastilloDev@users.noreply.github.com>
2021-05-25 22:17:04 -0500

    :gear: LIMPIEZA DE PROYECTO

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,9 @@
 <template>
-  <hello-world />
+  <div></div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+export default {
+  name: "Home",
+};
 </script>
