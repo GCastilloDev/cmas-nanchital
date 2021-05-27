38639e04175c2152358fc41c95b473a96fb70467
Gustavo Castillo N <37945820+GustavoCastilloDev@users.noreply.github.com>
2021-05-26 20:53:52 -0500

    CONFIGURACION HEROKU

--- server.js
+++ server.js
@@ -1,23 +1,11 @@
-const express = require('express');
-const path = require('path');
-const history = require('connect-history-api-fallback');
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 
-const app = express();
+const app = express()
 
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-app.use(staticFileMiddleware);
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}));
-app.use(staticFileMiddleware);
-
-app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'));
-});
-
-var server = app.listen(process.env.PORT || 8088, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
-});
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log(`app is listening on port: ${port}`)
