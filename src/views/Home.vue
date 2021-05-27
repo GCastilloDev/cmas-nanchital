6d09ec78d8c6e02d059bfc8be76e0aae36aeefb5
Gustavo Castillo N <37945820+GustavoCastilloDev@users.noreply.github.com>
2021-05-27 15:51:00 -0500

    FUNCIONALIDAD DEL TIEMPO

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,23 @@
 <template>
   <div>
-    <div class="img__header">
-      <div class="img__content">
-        <h1 class="white--text">Nanchital</h1>
-        <h1 class="white--text">35 grados o cuantos grados quieres?</h1>
+    <div class="img__header white--text">
+      <div class="img__content pt-5 pr-5">
+        <h1 class="white--text text-h5 font-weight-bold mb-3">{{ ciudad }}</h1>
+        <div class="mb-3">
+          <p class="mb-0">Temperatura actual</p>
+          <div class="d-flex justify-end align-end">
+            <img :src="imagen" alt="" height="30px" />
+            <span class="font-weight-bold">{{ temperatura }}</span>
+          </div>
+        </div>
+
+        <div>
+          <p class="mb-0">Sensación térmica</p>
+          <div class="d-flex justify-end align-end">
+            <img :src="imagen" alt="" height="30px" />
+            <span class="font-weight-bold">{{ sensacionTermica }}</span>
+          </div>
+        </div>
       </div>
       <!-- <v-img  src="@/assets/header.jpg"></v-img> -->
     </div>
@@ -11,8 +25,35 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Home",
+  mounted() {
+    this.datosClima();
+  },
+  data: () => ({
+    ciudad: "",
+    temperatura: "",
+    sensacionTermica: "",
+    imagen: "",
+  }),
+  methods: {
+    async datosClima() {
+      try {
+        const { data } = await axios.get(
+          `http://api.openweathermap.org/data/2.5/weather?q=nanchital&units=metric&lang=es&appid=4dab257d25d0f100b931220c089613dc`
+        );
+
+        this.ciudad = data.name;
+        this.temperatura = `${data.main.temp}°С`;
+        this.sensacionTermica = `${data.main.feels_like}°С`;
+        this.imagen = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+      } catch (error) {
+        console.warn(error);
+      }
+    },
+  },
 };
 </script>
 
