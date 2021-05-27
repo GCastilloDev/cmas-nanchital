2eb77d3e7fff2b4491ad0daaed20f7e2ca07f450
Gustavo Castillo N <37945820+GustavoCastilloDev@users.noreply.github.com>
2021-05-26 19:45:33 -0500

    CSS IMAGEN CABECERA

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <div class="img__header">
       <div class="img__content">
         <h1 class="white--text">Nanchital</h1>
-        <h1 class="white--text">35 grados</h1>
+        <h1 class="white--text">35 grados o cuantos grados quieres?</h1>
       </div>
       <!-- <v-img  src="@/assets/header.jpg"></v-img> -->
     </div>
@@ -19,12 +19,19 @@ export default {
 <style lang="css" scoped>
 .img__header {
   background: url("../assets/header.jpg");
+  background-image: no-repeat;
+  background-image: fixed;
+  background-image: center;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
   height: 80vh;
   text-align: right;
 }
 
 .img__content {
-  background: rgba(0,0,0,0.5);
+  background: rgba(0, 0, 0, 0.5);
   height: 100%;
 }
 </style>
